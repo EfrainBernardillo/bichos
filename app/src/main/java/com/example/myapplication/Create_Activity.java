742ddb41df0795bb3253b3bff1a7e677498c049d
unknown <Admin@IHR80PBF031.continental.edu.pe>
2019-12-04 21:57:08 -0500

    Cmabio por el dev.0

--- app/src/main/java/com/example/myapplication/Create_Activity.java
+++ app/src/main/java/com/example/myapplication/Create_Activity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Create_Activity extends AppCompatActivity {
+
+
 }
